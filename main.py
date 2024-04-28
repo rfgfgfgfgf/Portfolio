0fe43b6a89519054b53c1575e7d751ffa4e71edd
Teymur <156570166+rfgfgfgfgf@users.noreply.github.com>
2024-04-28 12:52:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+#Импорт
+from flask import Flask, render_template,request, redirect
 
+
+
+app = Flask(__name__)
+
+#Запуск страницы с контентом
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+#Динамичные скиллы
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    return render_template('index.html', button_python=button_python)
+
+@app.route('/', methods=['POST'])
+def process_form2():
+    button_discord = request.form.get('button_discord')
+    return render_template('index.html', button_discord=button_discord)
+
+@app.route('/', methods=['POST'])
+def process_form3():
+    button_html = request.form.get('button_html')
+    return render_template('index.html', button_html=button_html)
+
+@app.route('/', methods=['POST'])
+def process_form4():
+    button_db = request.form.get('button_db')
+    return render_template('https://github.com/rfgfgfgfgf/diary-project.git', button_db=button_db)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
