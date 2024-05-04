af2adb4fafbfe7e290781a15d8b5571a81ee9ac0
Teymur <156570166+rfgfgfgfgf@users.noreply.github.com>
2024-05-04 11:44:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,22 +15,15 @@ def index():
 @app.route('/', methods=['POST'])
 def process_form():
     button_python = request.form.get('button_python')
-    return render_template('index.html', button_python=button_python)
-
-@app.route('/', methods=['POST'])
-def process_form2():
     button_discord = request.form.get('button_discord')
-    return render_template('index.html', button_discord=button_discord)
-
-@app.route('/', methods=['POST'])
-def process_form3():
     button_html = request.form.get('button_html')
-    return render_template('index.html', button_html=button_html)
-
-@app.route('/', methods=['POST'])
-def process_form4():
     button_db = request.form.get('button_db')
-    return render_template('https://github.com/rfgfgfgfgf/diary-project.git', button_db=button_db)
+    return render_template('index.html', button_python=button_python, 
+                           button_discord=button_discord,
+                            button_html=button_html,
+                             button_db=button_db )
+
+ 
 
 
 if __name__ == "__main__":
